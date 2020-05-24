178e53e732f36e3c4a4147ba6ae2e92697b198da
kate myer <kate.d.mangubat@gmail.com>
2020-05-23 22:06:40 -0700

    added users controller test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 describe UsersController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  it "can get the Users index" do
+    get users_path
+
+    must_respond_with :redirect
+  end
 end
