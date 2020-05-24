f5d585c7a73123814fa6f1e7f624e204c94721b6
kate myer <kate.d.mangubat@gmail.com>
2020-05-23 22:05:31 -0700

    added get vote count method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
+
+  #validations
+  validates :username, presence: true
+
   has_many :votes, dependent: :destroy 
+  
+  def get_vote_count
+    #filter Vote Db by work_id = this.id
+    count = Vote.where(user_id: self.id).count
+    return count
+  end
+
 end
