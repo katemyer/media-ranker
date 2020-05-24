ef23bfe3a0d108bd50ab296f60209b067dbb69f1
kate myer <kate.d.mangubat@gmail.com>
2020-05-23 20:53:47 -0700

    updated votes relations

--- db/migrate/20200523061632_create_votes.rb
+++ db/migrate/20200523061632_create_votes.rb
@@ -1,8 +1,8 @@
 class CreateVotes < ActiveRecord::Migration[6.0]
   def change
     create_table :votes do |t|
-      t.integer :user_id
-      t.integer :work_id
+      t.belongs_to :work
+      t.belongs_to :user
 
       t.timestamps
     end
