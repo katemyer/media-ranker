ae56989fde766474a06866929542dd1d0d389945
kate myer <kate.d.mangubat@gmail.com>
2020-05-23 22:07:24 -0700

    added work model test

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 describe Work do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  let (:new_work) {
+    Work.new(category: "album", title: "Let it burn", creator: "Usher", publication_year: "2000", description: "best running album")
+  }
+  it "can be instantiated" do
+    # Assert
+    expect(new_work.valid?).must_equal true
+  end
+
+  it "will have the required fields" do
+    # Arrange
+    new_work.save
+    work = Work.first
+    [:category, :title, :creator, :publication_year, :description].each do |field|
+
+      # Assert
+      expect(work).must_respond_to field
+    end
+  end
 end
