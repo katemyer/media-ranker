a574bb8064c576981d1fd530ffb9526be9b4847f
kate myer <kate.d.mangubat@gmail.com>
2020-05-23 22:07:10 -0700

    added user model test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 describe User do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  let (:new_user) {
+    User.new(username: "test_user")
+  }
+  it "can be instantiated" do
+    # Assert
+    expect(new_user.valid?).must_equal true
+  end
+  
+  it "will have the required fields" do
+    # Arrange
+    new_user.save
+    found_user= User.find(new_user.id)
+
+      # Assert
+      expect(found_user.username).must_equal "test_user"
+  end
 end
